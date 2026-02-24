925e06c0d2a7eabe4e1e434fdc640718bf7129a2
TheSpace-hub <biometer2020@gmail.com>
2026-02-24 17:24:22 +0300

    Add color field

--- app/src/main/java/ru/seabattleroyal/game/Player.java
+++ app/src/main/java/ru/seabattleroyal/game/Player.java
@@ -3,13 +3,18 @@ package ru.seabattleroyal.game;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 @Getter
 @Setter
 public class Player {
 
+    private static final Random random = new Random();
+
     String username;
+    int color;
     UUID uuid;
     Field field;
 
@@ -17,6 +22,7 @@ public class Player {
         this.username = username;
         this.uuid = UUID.randomUUID();
         this.field = new Field(Field.CellType.UNKNOWN);
+        this.color = List.of(0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff).get(random.nextInt(0, 5));
     }
 
 }
