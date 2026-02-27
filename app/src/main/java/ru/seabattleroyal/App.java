7c412a1b219078e8654e29104bdf90d8e017a61c
TheSpace-hub <biometer2020@gmail.com>
2026-02-27 13:15:37 +0300

    Remove import

--- app/src/main/java/ru/seabattleroyal/App.java
+++ app/src/main/java/ru/seabattleroyal/App.java
@@ -1,7 +1,6 @@
 package ru.seabattleroyal;
 
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
